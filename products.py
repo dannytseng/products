8c7ddc7fdc7d70a9e1dd7bd51233567b7d6aa211
Danny Tseng <itidannytseng@gmail.com>
2019-11-20 00:57:23 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,19 @@
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	products.append([name, price])
+	# 其他第1種
+	# p = []
+	# p.append(name)
+	# p.append(price)
+	
+	# 其他第2種
+	#p = [name, price]
+	#products.append(p)
+	
+print(products)
+
+products[0][0]
